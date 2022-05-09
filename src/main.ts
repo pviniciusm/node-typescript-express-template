c03161f0ea31f1eeb979256ba6386edda6157f23
pviniciusm <pvinics@gmail.com>
2022-05-09 10:57:38 -0300

    add typeorm config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import express from 'express';
-import cors from 'cors';
+import { DatabaseConnection } from "./core/infra/database/connections/database-connection";
+import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(8081, () => console.log("Server is running..."));
+DatabaseConnection.initialize().then(() => {
+    app.listen(8081, () => console.log("Server is running..."));
+});
